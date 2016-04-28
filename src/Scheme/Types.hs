0a601afa9fa577ce3a89f137c429544fbafb7931
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-04-28 20:50:25 +0900

    Remove an unused LispError value constructor

--- src/Scheme/Types.hs
+++ src/Scheme/Types.hs
@@ -62,7 +62,6 @@ data LispError = NumArgs Integer [LispVal]
                | BadSpecialForm String LispVal
                | NotFunction String String
                | UnboundVar String String
-               | Default String
 
 showError :: LispError -> String
 showError (UnboundVar message varname) = message ++ ": " ++ varname
